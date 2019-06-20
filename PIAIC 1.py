5fb9d264f14a2f56e66840e6f23ee2326ce5a924
Mufazzil A Memon <mufazzilayazmemon@gmail.com>
2019-06-20 19:54:14 +0500

    Pyhton test file uploaded

---
+++ PIAIC 1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+print("hello\t world")
+
+
+# In[8]:
+
+
+a=7
+b=8
+print(a*b)
+
+
+# In[12]:
+
+
+a=5
+b=7
+c=a*b
+print(c)
+
+
+# In[14]:
+
+
+x='hassaan'
+print(x)
+
+
+# In[2]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
